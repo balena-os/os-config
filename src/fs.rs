9de4dfbf32f6c9b5c1cdf1983772ecb550e178b4
Zahari Petkov <zahari@balena.io>
2023-05-03 17:56:45 +0300

    Upgrade actix-web to 4.2 and fatrw to latest
    Change-type: patch
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/fs.rs
+++ src/fs.rs
@@ -1,19 +1,19 @@
 use std::path::Path;
 use std::u32;
 
-use fatrw::read_file as fatrw_read_file;
-use fatrw::write_file as fatrw_write_file;
+use fatrw::read::read_file as fatrw_read_file;
+use fatrw::write::write_file as fatrw_write_file;
 
 use anyhow::{Context, Result};
 
 pub fn read_file(path: &Path) -> Result<String> {
-    Ok(String::from_utf8(fatrw_read_file(path).context(
+    Ok(String::from_utf8(fatrw_read_file(path, false).context(
         format!("Reading {:?} failed", path.to_path_buf()),
     )?)?)
 }
 
 pub fn write_file(path: &Path, contents: &str, mode: Option<u32>) -> Result<()> {
-    fatrw_write_file(path, contents.as_bytes(), mode)
+    fatrw_write_file(path, contents.as_bytes(), mode, false)
         .context(format!("Writing {:?} failed", path.to_path_buf()))
 }
 
